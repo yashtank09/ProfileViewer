2f7e87e8a79b2615c571f9fa2a450d4185c1ce62
Yash Tank <62990171+yashtank09@users.noreply.github.com>
2021-11-29 22:56:54 +0530

    created a data entry point but can't get age dynamically in select tag

--- ProfileView/views.py
+++ ProfileView/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
 
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    context = {
+        'abc': range(100)
+    }
+    return render(request, 'ProfileView/index.html')
